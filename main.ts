aee7aa0a06f0e2c81506a268e6d533193466d67e
RyanAlvesz <ra546038@gmail.com>
2024-08-15 12:10:17 -0300

    feat: starting code

---
+++ main.ts
@@ -0,0 +1,26 @@
+console.log('oi');
+
+// Tipando variáveis
+let nome: string
+let idade: number
+let maiorIdade: boolean
+
+// Atribuindo valores
+nome = 'Ryan'
+idade = 18
+maiorIdade = true
+
+// Tipando ARRAY
+let lista: string[]
+lista = ['Ryan']
+
+let pessoa: [string, number, boolean]
+pessoa = [nome, idade, maiorIdade]
+
+// Tipando Objetos
+
+let objeto: object
+objeto = {
+    nome: 'Ryan',
+    idade: 18
+}
